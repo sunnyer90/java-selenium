5d535f6a5f03962f7ea28f10f77e879b5b411e49
Sunny Mittal <smittal16@sapient.com>
2020-12-17 11:56:01 +0530

    webdriver manager integration

--- src/main/java/conf/base.java
+++ src/main/java/conf/base.java
@@ -12,6 +12,13 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class base {
 	
@@ -25,14 +32,19 @@ public class base {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")) {
-			
-			System.setProperty("webdriver.chrome.driver", ".//src//main//java//conf//chromedriver");
-			driver = new ChromeDriver();
+			ChromeOptions chromeOptions = new ChromeOptions();
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver(chromeOptions);
 			
 		}else if(browserName.equals("firefox")) {
+			FirefoxOptions firefoxOptions = new FirefoxOptions();
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver(firefoxOptions);
 			
-		}else if(browserName.equals("IE")){
-			
+		}else if(browserName.equals("ie")){
+			InternetExplorerOptions ieOptions = new InternetExplorerOptions();
+			WebDriverManager.iedriver().setup();
+			driver = new InternetExplorerDriver(ieOptions);
 		}
 		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
